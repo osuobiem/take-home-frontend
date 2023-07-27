ce77ee923e5f78ea23b468142eaa7e86be26d3ea
Gabriel Osuobiem <osuobiem@gmail.com>
2023-07-27 14:32:42 +0100

    add router and login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {ReactQueryDevtools} from "react-query/devtools";
+import {RouterProvider} from "react-router-dom";
+import router from "./router.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </React.StrictMode>
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,5 @@
+import * as S from "./Login.style";
+
+export const Login = () => {
+  return <S.Container></S.Container>;
+};
